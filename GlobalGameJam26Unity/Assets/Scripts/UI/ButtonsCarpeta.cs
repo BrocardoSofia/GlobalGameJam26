f10a764a462f332087132573a14685105f17a6a1
Sofia Brocardo <sofia.brocardo@hotmail.com>
2026-02-01 09:11:05 -0300

    update

--- GlobalGameJam26Unity/Assets/Scripts/UI/ButtonsCarpeta.cs
+++ GlobalGameJam26Unity/Assets/Scripts/UI/ButtonsCarpeta.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ButtonsCarpeta : MonoBehaviour
 {
     public PolygraphController polygraphController;
     public LogicaPreguntas logicaPreguntas;
     public GameObject canvasCarpeta;
+    public GameObject botonIzquierdo;
+    public GameObject botonDerecho;
+    public UnityEngine.UI.Image fotoPlace;
+    public Sprite[] imagenes;
+    private int indice = 0;
 
     public void AbrirCarpeta()
     {
@@ -18,5 +25,29 @@ public class ButtonsCarpeta : MonoBehaviour
         polygraphController.SetPolygraphState("start");
         canvasCarpeta.SetActive(false);
         logicaPreguntas.Reanudar();
+    }
+
+    public void PasarImagen(string hacia)
+    {
+        if(hacia == "Derecha")
+        {
+            fotoPlace.sprite = imagenes[indice + 1];
+            indice++;
+            if(indice == imagenes.Length)
+            {
+                botonDerecho.SetActive(false);
+            }
+            botonIzquierdo.SetActive(true);
+        }
+        else
+        {
+            fotoPlace.sprite = imagenes[indice -1];
+            indice--;
+            if(indice == 0)
+            {
+                botonIzquierdo.SetActive(false);
+            }
+            botonDerecho.SetActive(true);
+        }
     }
 }
